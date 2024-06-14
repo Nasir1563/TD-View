2cfe080b3c030599a54f095e115f702244e25115
Nasir1563 <nasirhs850@gmail.com>
2024-06-15 01:04:21 +0600

    Update app.py

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def max_consecutive(trades):
     return max_count
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
